e26776de841292ac55a18c7f35decbfebe9544ff
Tay <tyet@f8ffc247f36a.ant.amazon.com>
2020-09-15 15:27:09 +0800

    update to call instance ID

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,64 @@
 var express = require('express')
+
 var app = express()
 
+	
+
 // SHOW LIST OF USERS
-app.get('/', function(req, res, next) {
-	req.getConnection(function(error, conn) {
-		conn.query('SELECT * FROM users ORDER BY id DESC',function(err, rows, fields) {
+app.get('/', function (req, res, next) {
+	req.getConnection(function (error, conn) {
+		conn.query('SELECT * FROM users ORDER BY id DESC', function (err, rows, fields) {
 			//if(err) throw err
 			if (err) {
 				req.flash('error', err)
 				res.render('user/list', {
-					title: 'User List', 
+					title: 'User List',
 					data: ''
 				})
 			} else {
 				// render to views/user/list.ejs template file
-				res.render('user/list', {
-					title: 'User List', 
-					data: rows
-				})
-			}
+				var AWS = require('aws-sdk');
+				var meta = new AWS.MetadataService();
+				var instanceInfo = 'Local Computer';
+
+				meta.request("/latest/meta-data/instance-id", function (err, data) {
+					if (err) {
+						instanceInfo = 'Local Computer';
+					} else {
+						instanceInfo = "instance ID" + data;
+					}
+				});
+					res.render('user/list', {
+						title: 'User List',
+						data: rows,
+						instanceDetails: instanceInfo
+					})
+				}
 		})
 	})
 })
 
 // SHOW ADD USER FORM
-app.get('/add', function(req, res, next){	
+app.get('/add', function (req, res, next) {
 	// render to views/user/add.ejs
 	res.render('user/add', {
 		title: 'Add New User',
 		name: '',
 		age: '',
-		email: ''		
+		email: ''
 	})
 })
 
 // ADD NEW USER POST ACTION
-app.post('/add', function(req, res, next){	
+app.post('/add', function (req, res, next) {
 	req.assert('name', 'Name is required').notEmpty()           //Validate name
 	req.assert('age', 'Age is required').notEmpty()             //Validate age
-    req.assert('email', 'A valid email is required').isEmail()  //Validate email
+	req.assert('email', 'A valid email is required').isEmail()  //Validate email
 
-    var errors = req.validationErrors()
-    
-    if( !errors ) {   //No errors were found.  Passed Validation!
-		
+	var errors = req.validationErrors()
+
+	if (!errors) {   //No errors were found.  Passed Validation!
+
 		/********************************************
 		 * Express-validator module
 		 
@@ -58,29 +73,29 @@ app.post('/add', function(req, res, next){
 			age: req.sanitize('age').escape().trim(),
 			email: req.sanitize('email').escape().trim()
 		}
-		
-		req.getConnection(function(error, conn) {
-			conn.query('INSERT INTO users SET ?', user, function(err, result) {
+
+		req.getConnection(function (error, conn) {
+			conn.query('INSERT INTO users SET ?', user, function (err, result) {
 				//if(err) throw err
 				if (err) {
 					req.flash('error', err)
-					
+
 					// render to views/user/add.ejs
 					res.render('user/add', {
 						title: 'Add New User',
 						name: user.name,
 						age: user.age,
-						email: user.email					
+						email: user.email
 					})
-				} else {				
+				} else {
 					req.flash('success', 'Data added successfully!')
-					
+
 					// render to views/user/add.ejs
 					res.render('user/add', {
 						title: 'Add New User',
 						name: '',
 						age: '',
-						email: ''					
+						email: ''
 					})
 				}
 			})
@@ -88,30 +103,30 @@ app.post('/add', function(req, res, next){
 	}
 	else {   //Display errors to user
 		var error_msg = ''
-		errors.forEach(function(error) {
+		errors.forEach(function (error) {
 			error_msg += error.msg + '<br>'
-		})				
-		req.flash('error', error_msg)		
-		
+		})
+		req.flash('error', error_msg)
+
 		/**
 		 * Using req.body.name 
 		 * because req.param('name') is deprecated
-		 */ 
-        res.render('user/add', { 
-            title: 'Add New User',
-            name: req.body.name,
-            age: req.body.age,
-            email: req.body.email
-        })
-    }
+		 */
+		res.render('user/add', {
+			title: 'Add New User',
+			name: req.body.name,
+			age: req.body.age,
+			email: req.body.email
+		})
+	}
 })
 
 // SHOW EDIT USER FORM
-app.get('/edit/(:id)', function(req, res, next){
-	req.getConnection(function(error, conn) {
-		conn.query('SELECT * FROM users WHERE id = ?', [req.params.id], function(err, rows, fields) {
-			if(err) throw err
-			
+app.get('/edit/(:id)', function (req, res, next) {
+	req.getConnection(function (error, conn) {
+		conn.query('SELECT * FROM users WHERE id = ?', [req.params.id], function (err, rows, fields) {
+			if (err) throw err
+
 			// if user not found
 			if (rows.length <= 0) {
 				req.flash('error', 'User not found with id = ' + req.params.id)
@@ -120,28 +135,28 @@ app.get('/edit/(:id)', function(req, res, next){
 			else { // if user found
 				// render to views/user/edit.ejs template file
 				res.render('user/edit', {
-					title: 'Edit User', 
+					title: 'Edit User',
 					//data: rows[0],
 					id: rows[0].id,
 					name: rows[0].name,
 					age: rows[0].age,
-					email: rows[0].email					
+					email: rows[0].email
 				})
-			}			
+			}
 		})
 	})
 })
 
 // EDIT USER POST ACTION
-app.put('/edit/(:id)', function(req, res, next) {
+app.put('/edit/(:id)', function (req, res, next) {
 	req.assert('name', 'Name is required').notEmpty()           //Validate name
 	req.assert('age', 'Age is required').notEmpty()             //Validate age
-    req.assert('email', 'A valid email is required').isEmail()  //Validate email
+	req.assert('email', 'A valid email is required').isEmail()  //Validate email
 
-    var errors = req.validationErrors()
-    
-    if( !errors ) {   //No errors were found.  Passed Validation!
-		
+	var errors = req.validationErrors()
+
+	if (!errors) {   //No errors were found.  Passed Validation!
+
 		/********************************************
 		 * Express-validator module
 		 
@@ -156,13 +171,13 @@ app.put('/edit/(:id)', function(req, res, next) {
 			age: req.sanitize('age').escape().trim(),
 			email: req.sanitize('email').escape().trim()
 		}
-		
-		req.getConnection(function(error, conn) {
-			conn.query('UPDATE users SET ? WHERE id = ' + req.params.id, user, function(err, result) {
+
+		req.getConnection(function (error, conn) {
+			conn.query('UPDATE users SET ? WHERE id = ' + req.params.id, user, function (err, result) {
 				//if(err) throw err
 				if (err) {
 					req.flash('error', err)
-					
+
 					// render to views/user/add.ejs
 					res.render('user/edit', {
 						title: 'Edit User',
@@ -173,7 +188,7 @@ app.put('/edit/(:id)', function(req, res, next) {
 					})
 				} else {
 					req.flash('success', 'Data updated successfully!')
-					
+
 					// render to views/user/add.ejs
 					res.render('user/edit', {
 						title: 'Edit User',
@@ -188,31 +203,31 @@ app.put('/edit/(:id)', function(req, res, next) {
 	}
 	else {   //Display errors to user
 		var error_msg = ''
-		errors.forEach(function(error) {
+		errors.forEach(function (error) {
 			error_msg += error.msg + '<br>'
 		})
 		req.flash('error', error_msg)
-		
+
 		/**
 		 * Using req.body.name 
 		 * because req.param('name') is deprecated
-		 */ 
-        res.render('user/edit', { 
-            title: 'Edit User',            
-			id: req.params.id, 
+		 */
+		res.render('user/edit', {
+			title: 'Edit User',
+			id: req.params.id,
 			name: req.body.name,
 			age: req.body.age,
 			email: req.body.email
-        })
-    }
+		})
+	}
 })
 
 // DELETE USER
-app.delete('/delete/(:id)', function(req, res, next) {
+app.delete('/delete/(:id)', function (req, res, next) {
 	var user = { id: req.params.id }
-	
-	req.getConnection(function(error, conn) {
-		conn.query('DELETE FROM users WHERE id = ' + req.params.id, user, function(err, result) {
+
+	req.getConnection(function (error, conn) {
+		conn.query('DELETE FROM users WHERE id = ' + req.params.id, user, function (err, result) {
 			//if(err) throw err
 			if (err) {
 				req.flash('error', err)
